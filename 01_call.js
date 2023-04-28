7459b738699f46c0f258e9a4a9f318a7a550b692
Aipor <774434463@qq.com>
2023-04-28 11:04:19 +0800

    node环境下的this

--- 01_call.js
+++ 01_call.js
@@ -4,14 +4,14 @@
  * @Author: Aipor
  * @Date: 2023-04-28 10:01:44
  * @LastEditors: Aipor
- * @LastEditTime: 2023-04-28 10:59:20
+ * @LastEditTime: 2023-04-28 11:03:51
  */
 Function.prototype.gytCall = function (thisArg, ...argArry) {
   // 对传入的thisArg的对象进行判断，防止传入数据为非对象类型，默认绑定全局环境
   const thisObj =
     thisArg === undefined || thisArg === null
       ? typeof process !== undefined
-        ? {}
+        ? process
         : window
       : Object(thisArg);
   const symbolFn = Symbol("fn");
@@ -31,6 +31,7 @@ const obj = {
   name: "obj",
 };
 bar.call(obj, "arg1", "arg2");
+bar.call(null);
 bar.gytCall(obj, "arg1", "arg2");
 bar.gytCall(null);
   
